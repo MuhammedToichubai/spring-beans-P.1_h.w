e8ae168155bccfed04fc45ea4c53e9d3a5f3f185
Muhammed <muhammedtoichubai@gmail.com>
2022-05-14 01:06:00 -0600

    practice N1

--- src/main/java/peaksoft/config/AppConfig.java
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -1,9 +1,14 @@
 package peaksoft.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import peaksoft.model.Animal;
+import peaksoft.model.Dog;
+import peaksoft.model.Timer;
 
 @Configuration
 @ComponentScan(basePackages = "peaksoft")
 public class AppConfig {
+
 }
